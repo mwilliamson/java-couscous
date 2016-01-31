15dd7cff820099f4d7a3d74b9556b32bd2f77cf1
Michael Williamson <mike@zwobble.org>
2016-01-31 18:56:07 +0000

    Enable passing test in C# backend

--- src/test/java/org/zwobble/couscous/tests/backends/csharp/CsharpCompilerEvalTests.java
+++ src/test/java/org/zwobble/couscous/tests/backends/csharp/CsharpCompilerEvalTests.java
@@ -1,7 +1,5 @@
 package org.zwobble.couscous.tests.backends.csharp;
 
-import org.junit.Ignore;
-import org.junit.Test;
 import org.zwobble.couscous.ast.ClassNode;
 import org.zwobble.couscous.ast.ExpressionNode;
 import org.zwobble.couscous.tests.BackendEvalTests;
@@ -10,11 +8,6 @@ import org.zwobble.couscous.values.PrimitiveValue;
 import java.util.List;
 
 public class CsharpCompilerEvalTests extends BackendEvalTests {
-    @Test
-    @Ignore
-    public void canCallStaticMethodFromUserDefinedStaticMethod() {
-    }
-
     @Override
     protected PrimitiveValue evalExpression(List<ClassNode> classes, ExpressionNode expression) {
         return new CsharpMethodRunner().evalExpression(classes, expression);
