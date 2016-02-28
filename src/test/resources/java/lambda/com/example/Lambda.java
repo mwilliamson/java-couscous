6a203656132e2acdfb73d8e84f5fa03e9ce068be
Michael Williamson <mike@zwobble.org>
2016-02-28 18:14:46 +0000

    Modify lambda test to run on C# backend

--- src/test/resources/java/lambda/com/example/Lambda.java
+++ src/test/resources/java/lambda/com/example/Lambda.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 
 public class Lambda {
     public static int value() {
-        Function<Integer, Integer> function = value -> value * 2;
+        IntFunction function = value -> value * 2;
         return function.apply(21);
     }
 }
