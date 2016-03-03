e92e300f3c25e7c003f7db189779c4a8d3309753
Michael Williamson <mike@zwobble.org>
2016-03-03 19:24:41 +0000

    Remove unused code from MethodValue

--- src/main/java/org/zwobble/couscous/interpreter/values/MethodValue.java
+++ src/main/java/org/zwobble/couscous/interpreter/values/MethodValue.java
@@ -1,9 +1,10 @@
 package org.zwobble.couscous.interpreter.values;
 
-import java.util.List;
-import java.util.function.BiFunction;
 import org.zwobble.couscous.ast.TypeName;
 import org.zwobble.couscous.interpreter.Environment;
+
+import java.util.List;
+import java.util.function.BiFunction;
 
 public final class MethodValue implements Callable {
     private final List<TypeName> argumentTypes;
@@ -20,35 +21,5 @@ public final class MethodValue implements Callable {
     
     public List<TypeName> getArgumentTypes() {
         return this.argumentTypes;
-    }
-    
-    @java.lang.Override
-    public boolean equals(final java.lang.Object o) {
-        if (o == this) return true;
-        if (!(o instanceof MethodValue)) return false;
-        final MethodValue other = (MethodValue)o;
-        final java.lang.Object this$argumentTypes = this.getArgumentTypes();
-        final java.lang.Object other$argumentTypes = other.getArgumentTypes();
-        if (this$argumentTypes == null ? other$argumentTypes != null : !this$argumentTypes.equals(other$argumentTypes)) return false;
-        final java.lang.Object this$apply = this.apply;
-        final java.lang.Object other$apply = other.apply;
-        if (this$apply == null ? other$apply != null : !this$apply.equals(other$apply)) return false;
-        return true;
-    }
-    
-    @java.lang.Override
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final java.lang.Object $argumentTypes = this.getArgumentTypes();
-        result = result * PRIME + ($argumentTypes == null ? 43 : $argumentTypes.hashCode());
-        final java.lang.Object $apply = this.apply;
-        result = result * PRIME + ($apply == null ? 43 : $apply.hashCode());
-        return result;
-    }
-    
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "MethodValue(argumentTypes=" + this.getArgumentTypes() + ", apply=" + this.apply + ")";
     }
 }
