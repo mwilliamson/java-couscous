7cfee2776b19caddfe1ee218f5df430ce42ac666
Michael Williamson <mike@zwobble.org>
2016-03-03 19:25:24 +0000

    Remove unused code from StaticMethodValue

--- src/main/java/org/zwobble/couscous/interpreter/values/StaticMethodValue.java
+++ src/main/java/org/zwobble/couscous/interpreter/values/StaticMethodValue.java
@@ -1,10 +1,11 @@
 package org.zwobble.couscous.interpreter.values;
 
+import org.zwobble.couscous.ast.TypeName;
+import org.zwobble.couscous.interpreter.Environment;
+import org.zwobble.couscous.interpreter.PositionalArguments;
+
 import java.util.List;
 import java.util.function.BiFunction;
-import org.zwobble.couscous.ast.TypeName;
-import org.zwobble.couscous.interpreter.PositionalArguments;
-import org.zwobble.couscous.interpreter.Environment;
 
 public final class StaticMethodValue implements Callable {
     private final List<TypeName> argumentTypes;
@@ -21,35 +22,5 @@ public final class StaticMethodValue implements Callable {
     
     public List<TypeName> getArgumentTypes() {
         return this.argumentTypes;
-    }
-    
-    @java.lang.Override
-    public boolean equals(final java.lang.Object o) {
-        if (o == this) return true;
-        if (!(o instanceof StaticMethodValue)) return false;
-        final StaticMethodValue other = (StaticMethodValue)o;
-        final java.lang.Object this$argumentTypes = this.getArgumentTypes();
-        final java.lang.Object other$argumentTypes = other.getArgumentTypes();
-        if (this$argumentTypes == null ? other$argumentTypes != null : !this$argumentTypes.equals(other$argumentTypes)) return false;
-        final java.lang.Object this$apply = this.apply;
-        final java.lang.Object other$apply = other.apply;
-        if (this$apply == null ? other$apply != null : !this$apply.equals(other$apply)) return false;
-        return true;
-    }
-    
-    @java.lang.Override
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final java.lang.Object $argumentTypes = this.getArgumentTypes();
-        result = result * PRIME + ($argumentTypes == null ? 43 : $argumentTypes.hashCode());
-        final java.lang.Object $apply = this.apply;
-        result = result * PRIME + ($apply == null ? 43 : $apply.hashCode());
-        return result;
-    }
-    
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "StaticMethodValue(argumentTypes=" + this.getArgumentTypes() + ", apply=" + this.apply + ")";
     }
 }
